1757fe69a01f4242d28b0fa7772737e64abb3aad
sixpakstrad <sixpakstrad@gmail.com>
2026-08-28 14:33:48 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    if (process.env.VERCEL_ENV !== "production") {
-      return [];
-    }
-
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Robots-Tag",
-            value: "index, follow",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
